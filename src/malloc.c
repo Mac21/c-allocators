b7ad32972b3f9fdc7f54f1f068f0bc771ddc2e5c
Mac21 <russ.tolle@gmail.com>
2017-12-30 01:29:58 -0700

    Initial commit.

---
+++ src/malloc.c
@@ -0,0 +1,13 @@
+#include "malloc.h"
+#include "stddef.h"
+
+struct FREE_BLK {
+};
+
+typedef struct FREE_BLK free_blk_t;
+
+free_blk_t FREE_LIST[100];
+
+void* malloc(size_t size) {
+  return (void*)&size;
+}
---
+++ src/malloc.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "stddef.h"
+
+void* malloc(size_t size);
+void* realloc(void* mem_ptr, size_t size);
